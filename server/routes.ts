8618364d0238a6a90eb14880d33484cd568b9249
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-07-12 13:34:08 -0300

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -26,11 +26,20 @@ router.get("/system/health", (req, res) => {
     status: "OK",
     deployDate: SERVER_START_DATE,
     envs: {
-      COMMIT_HASH: process.env.COMMIT_HASH,
+      COMMIT_HASH: process.env.COMMIT_HASH ? process.env.COMMIT_HASH : "NOT SET",
       NODE_ENV: process.env.NODE_ENV,
+      INSTANCE_PROTOCOL: process.env.INSTANCE_PROTOCOL,
       INSTANCE_DOMAIN: process.env.INSTANCE_DOMAIN,
       INTERACTIVE_KEY: process.env.INTERACTIVE_KEY,
+      INTERACTIVE_SECRET: process.env.INTERACTIVE_SECRET ? "SET" : "NOT SET",
+      DEFAULT_THEME: process.env.DEFAULT_THEME ? process.env.DEFAULT_THEME : "NOT SET",
       S3_BUCKET: process.env.S3_BUCKET,
+      GOOGLESHEETS_CLIENT_EMAIL: process.env.GOOGLESHEETS_CLIENT_EMAIL ? "SET" : "NOT SET",
+      GOOGLESHEETS_SHEET_ID: process.env.GOOGLESHEETS_SHEET_ID ? "SET" : "NOT SET",
+      GOOGLESHEETS_PRIVATE_KEY: process.env.GOOGLESHEETS_PRIVATE_KEY ? "SET" : "NOT SET",
+      SCENE_ID_0: process.env.SCENE_ID_0 ? process.env.SCENE_ID_0 : "NOT SET",
+      DROPPABLE_SCENE_IDS_0: process.env.DROPPABLE_SCENE_IDS_0 ? process.env.DROPPABLE_SCENE_IDS_0 : "NOT SET",
+      ANCHOR_ASSET_IMAGE_0: process.env.ANCHOR_ASSET_IMAGE_0 ? process.env.ANCHOR_ASSET_IMAGE_0 : "NOT SET",
     },
   });
 });
