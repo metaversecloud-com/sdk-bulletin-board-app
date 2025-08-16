30115654da4132d33b1fc23a502d091d51d37eaa
Lina <linabell@mail.com>
2025-08-15 18:02:23 -0700

    additional world data object clean up

--- server/utils/initializeDataObject.ts
+++ server/utils/initializeDataObject.ts
@@ -2,6 +2,7 @@ import { getAnchorAssets, getThemeEnvVars, World } from "./index.js";
 import { Credentials, DataObjectType, IDroppedAsset } from "../types.js";
 
 type WorldDataObject = {
+  missingScenesDeleted?: boolean;
   scenes: {
     [key: string]: DataObjectType;
   };
@@ -21,7 +22,7 @@ export const initializeDataObject = async ({
 
     if (keyAsset.dataObject?.theme?.id) return;
 
-    let payload: DataObjectType = {
+    let keyAssetPayload: DataObjectType = {
       anchorAssets: [],
       messages: {},
       theme: {
@@ -40,35 +41,50 @@ export const initializeDataObject = async ({
     await world.fetchDataObject();
 
     let worldDataObject = world.dataObject as WorldDataObject;
+    const worldPayload = worldDataObject as any;
+    let shouldUpdateWorldDataObject = false;
+
+    if (!worldPayload?.missingScenesDeleted) {
+      await world.fetchScenes();
+      if (world.scenes) {
+        const worldScenes: { [key: string]: any } = world.scenes;
+        for (const sceneId of Object.keys(worldPayload.scenes)) {
+          if (!worldScenes[sceneId]) {
+            delete worldDataObject.scenes[sceneId];
+            shouldUpdateWorldDataObject = true;
+          }
+        }
+      }
+    }
 
     if (!worldDataObject?.scenes?.[sceneDropId]?.theme) {
       const themeId = keyAsset.dataObject?.themeId || process.env.DEFAULT_THEME || "CHALK";
       const getThemeResult = await getThemeEnvVars(themeId);
       if (getThemeResult instanceof Error) throw getThemeResult;
-      payload.theme = getThemeResult.theme;
+      keyAssetPayload.theme = getThemeResult.theme;
 
       const getAnchorsResult = await getAnchorAssets(credentials);
       if (getAnchorsResult instanceof Error) throw getAnchorsResult;
-      payload.anchorAssets = getAnchorsResult.anchorAssetIds;
+      keyAssetPayload.anchorAssets = getAnchorsResult.anchorAssetIds;
     } else {
       // existing scene data object found in world, transfer to key asset's data object
-      payload = worldDataObject.scenes[sceneDropId];
-
-      await world.updateDataObject(
-        {
-          [`scenes.${sceneDropId}`]: `Data transferred to key asset on ${new Date()}`,
-        },
-        { lock: { lockId, releaseLock: true } },
-      );
+      keyAssetPayload = worldDataObject.scenes[sceneDropId];
+      shouldUpdateWorldDataObject = true;
     }
 
     if (keyAsset.dataObject) {
-      await keyAsset.updateDataObject({ ...payload }, { lock: { lockId, releaseLock: true } });
+      await keyAsset.updateDataObject({ ...keyAssetPayload }, { lock: { lockId, releaseLock: true } });
     } else {
-      await keyAsset.setDataObject({ ...payload }, { lock: { lockId, releaseLock: true } });
+      await keyAsset.setDataObject({ ...keyAssetPayload }, { lock: { lockId, releaseLock: true } });
     }
 
     await keyAsset.fetchDataObject();
+
+    if (shouldUpdateWorldDataObject) {
+      worldPayload.missingScenesDeleted = true;
+      worldPayload.scenes[sceneDropId] = `Data transferred to key asset on ${new Date()}`;
+      world.updateDataObject(worldPayload, { lock: { lockId, releaseLock: true } });
+    }
 
     return;
   } catch (error: any) {
